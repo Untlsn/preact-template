0aee38354f1ba09334311c4386f46c0d12854e7f
Untel <Untellson@gmail.com>
2021-08-04 11:18:51 +0200

    Rollback some changes and add preact only on production (preact refresh dont work well with styled-components)

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
 import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
 import { resolve } from 'path';
+import refresh from '@vitejs/plugin-react-refresh';
 
 const reactCompat = resolve(__dirname, 'node_modules/preact/compat');
 
+const aliases = () => {
+  const def = { '~': resolve(__dirname, 'src') };
+  const production = {
+    'react': reactCompat,
+    'react-dom': reactCompat,
+  };
+
+  return process.env.NODE_ENV === 'production'
+    ? { ...def, ...production }
+    : def;
+};
+
 export default defineConfig({
-  plugins: [preact()],
   resolve: {
-    alias: {
-      '~': resolve(__dirname, 'src'),
-      'react': reactCompat,
-      'react-dom': reactCompat,
-    },
+    alias: aliases(),
   },
+  plugins: [refresh()],
 });
